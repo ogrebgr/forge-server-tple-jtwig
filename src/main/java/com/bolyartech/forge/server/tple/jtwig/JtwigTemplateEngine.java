66d43995bff01392677d120350125aff4ea3e508
Ognyan Bankov <ogibankov@gmail.com>
2017-02-02 08:40:43 +0200

    adding test page for the template engines

--- src/main/java/com/bolyartech/forge/server/tple/jtwig/JtwigTemplateEngine.java
+++ src/main/java/com/bolyartech/forge/server/tple/jtwig/JtwigTemplateEngine.java
@@ -3,6 +3,7 @@ package com.bolyartech.forge.server.tple.jtwig;
 import com.bolyartech.forge.server.misc.TemplateEngine;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
+import org.jtwig.environment.EnvironmentConfiguration;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +11,13 @@ import java.util.Map;
 
 public class JtwigTemplateEngine implements TemplateEngine {
     private final String mTemplatePathPrefix;
+    private final EnvironmentConfiguration mEnv;
     private final Map<String, Object> mAttributes = new HashMap<>();
 
 
-    public JtwigTemplateEngine(String templatePathPrefix) {
+    public JtwigTemplateEngine(String templatePathPrefix, EnvironmentConfiguration env) {
         mTemplatePathPrefix = templatePathPrefix;
+        mEnv = env;
     }
 
 
@@ -27,7 +30,7 @@ public class JtwigTemplateEngine implements TemplateEngine {
     @Override
     public String render(String templateName) {
         String viewName = mTemplatePathPrefix + templateName;
-        JtwigTemplate template = JtwigTemplate.classpathTemplate(viewName);
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(viewName, mEnv);
         JtwigModel model = JtwigModel.newModel(mAttributes);
         return template.render(model);
     }
--- src/main/java/com/bolyartech/forge/server/tple/jtwig/JtwigTemplateEngineFactory.java
+++ src/main/java/com/bolyartech/forge/server/tple/jtwig/JtwigTemplateEngineFactory.java
@@ -2,19 +2,29 @@ package com.bolyartech.forge.server.tple.jtwig;
 
 import com.bolyartech.forge.server.misc.TemplateEngine;
 import com.bolyartech.forge.server.misc.TemplateEngineFactory;
+import org.jtwig.environment.DefaultEnvironmentConfiguration;
+import org.jtwig.environment.EnvironmentConfiguration;
+import org.jtwig.parser.JtwigParser;
 
 
 public class JtwigTemplateEngineFactory implements TemplateEngineFactory {
     private final String mTemplatePathPrefix;
+    private final EnvironmentConfiguration mEnv;
+
+
+    public JtwigTemplateEngineFactory(String templatePathPrefix, EnvironmentConfiguration env) {
+        mTemplatePathPrefix = templatePathPrefix;
+        mEnv = env;
+    }
 
 
     public JtwigTemplateEngineFactory(String templatePathPrefix) {
-        mTemplatePathPrefix = templatePathPrefix;
+        this(templatePathPrefix, new DefaultEnvironmentConfiguration());
     }
 
 
     @Override
     public TemplateEngine createNew() {
-        return new JtwigTemplateEngine(mTemplatePathPrefix);
+        return new JtwigTemplateEngine(mTemplatePathPrefix, mEnv);
     }
 }
